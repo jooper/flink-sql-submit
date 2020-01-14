8837c86f378b4cb48fe57a6470c4412a4251f05c
edward.hbd <hbd_master@163.com>
2020-01-14 10:09:26 +0800

    调整对oracle表的etl的job名称

--- submitKf2Kf_Etl.sh
+++ submitKf2Kf_Etl.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 source "$(dirname "$0")"/env.sh
-
-
-
 #use case * $FLINK_DIR/bin/flink run -d -p 4 -c com.github.wuchong.ETL.ETL_OGG_DATA_SINKTO_KAFKA  target/flink-sql-submit.jar "sourceTopicId" "sinkTopicId" "jobname"
 #use case * $FLINK_DIR/bin/flink run -d -p 4 -c com.github.wuchong.ETL.ETL_OGG_DATA_SINKTO_KAFKA  target/flink-sql-submit.jar "$1" "$2" "$3"
+# 对ogg抽取到kafka中的数据进行抽取清洗，拿取after之后的数据放到相应的topic中
+topics=(TEST_OGG OPR_REGISTRATION)
 
+for topic in ${topics[@]}; do
+    if [ topic != "" ]; then
+        echo topic
+        $FLINK_DIR/bin/flink run -d -p 4 -c com.github.wuchong.ETL.ETL_OGG_DATA_SINKTO_KAFKA target/flink-sql-submit.jar "${topic}" "${topic}_AfterData" "ETL_ORACLE_TABLE_${topic}_JOB"
+    fi
+done
 
-$FLINK_DIR/bin/flink run -d -p 4 -c com.github.wuchong.ETL.ETL_OGG_DATA_SINKTO_KAFKA  target/flink-sql-submit.jar "TEST_OGG" "user_behavior" "ETL_ORACLE_TABLE_TEST_OGG_JOB"
-
+# $FLINK_DIR/bin/flink run -d -p 4 -c com.github.wuchong.ETL.ETL_OGG_DATA_SINKTO_KAFKA  target/flink-sql-submit.jar "TEST_OGG" "user_behavior" "ETL_ORACLE_TABLE_TEST_OGG_JOB"
