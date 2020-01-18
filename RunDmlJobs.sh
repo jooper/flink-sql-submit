a389d378ab02e1c1c2aae1b0ea81a43060710d64
edward.hbd <hbd_master@163.com>
2020-01-18 14:28:04 +0800

    重命名文件

--- RunDmlJobs.sh
+++ RunDmlJobs.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-#今日门诊药品费
-./run.sh "OPC_Today_Drug_Ypf" "Metric_Compute_Today_Drug_YP_FEE"
+##今日门诊药品费
+#./run.sh "METRIC_OPC_TODAY_DRUG_YPF_FEE" "Metric_Compute_Today_Drug_YP_FEE"
+#
+##今日门诊诊疗费
+#./run.sh "METRIC_OPC_TODAY_DIAG_ZLF_FEE" "Metric_Compute_Today_Diag_ZL_FEE"
 
-#今日门诊诊疗费
-./run.sh "OPC_Today_Diag_Zlf" "Metric_Compute_Today_Diag_ZL_FEE"
+
+
+#今日门诊诊疗费、今日门诊药品费
+./run.sh "METRIC_TODAY_ZLF_AND_YPF_FEE" "Metric_Compute_TODAY_ZLF_AND_YPF_FEE"
