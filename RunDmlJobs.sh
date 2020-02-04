739c67ba93ea4c8e49dba5a7de55b1caadc94d50
edward.hbd <hbd_master@163.com>
2020-02-04 14:44:03 +0800

    添加注解

--- RunDmlJobs.sh
+++ RunDmlJobs.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# 运行resource下的sql中的脚本，进行消费kafka中进行了ETL后的数据到mysql，在通过mysql的canal实时同步到kafka中，供ui的socketio访问
 
 ##今日门诊药品费
 #./run.sh "METRIC_OPC_TODAY_DRUG_YPF_FEE" "Metric_Compute_Today_Drug_YP_FEE"
